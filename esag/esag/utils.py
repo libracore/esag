9e1750348424a58c3a4942fd8487a6a8b744c75f
lasalesi <larsericmueller@gmail.com>
2023-02-07 11:21:57 +0100

    Update utils.py
    intermediate price function

--- esag/esag/utils.py
+++ esag/esag/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, libracore and Contributors
+# Copyright (c) 2022-2023, libracore and Contributors
 # License: GNU General Public License v3. See license.txt
 
 import frappe
@@ -54,3 +54,48 @@ def get_next_barcode():
     else:
         last_id = 0
     return "{0:014d}".format(last_id + 1)
+
+"""
+Calculate the intermediate price from two price lists and write to target price list
+"""
+@frappe.whitelist()
+def intermediate_price(item_code, ek_price_list, vk_price_list, target_price_list):
+    # fetch prices
+    prices = frappe.db.sql("""
+        SELECT 
+        `tabItem`.`item_code` AS `item_code`,
+        IFNULL((SELECT `price_list_rate` 
+         FROM `tabItem Price` 
+         WHERE `tabItem Price`.`item_code` = `tabItem`.`name` 
+           AND `tabItem Price`.`price_list` = "{ekp}"
+        ), 0) AS `ekp`,
+        IFNULL((SELECT `price_list_rate` 
+         FROM `tabItem Price` 
+         WHERE `tabItem Price`.`item_code` = `tabItem`.`name` 
+           AND `tabItem Price`.`price_list` = "{vkp}"
+        ), 0) AS `vkp`
+    FROM `tabItem`;
+    """, as_dict=True)
+    intermediate = (prices[0]['ekp'] + prices[0]['vkp'])/2
+    # check if the target price exists
+    target_prices = frappe.get_all("Item Price", 
+        filters={'item_code': item_code, 'price_list': target_price_list'}, 
+        fields=['name']
+    )
+    if len(target_prices) > 0:
+        # update
+        doc = frappe.get_doc("Item Price", target_prices[0]['name'])
+        doc.price_list_rate = intermediate
+        doc.save()
+    else:
+        # create
+        doc = frappe.get_doc({
+            'doctype': 'Item Price',
+            'item_code': item_code,
+            'price_list': target_price_list',
+            'price_list_rate': intermediate
+        })
+        doc.insert()
+    frappe.db.commit()
+    return
+    
