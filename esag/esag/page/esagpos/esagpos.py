5cb8ed701d4e8ddb193698bc24a492e4e636a225
Joelios <joel.mesmer@libracore.com>
2023-09-05 14:21:10 +0200

    search_field bugfix

--- esag/esag/page/esagpos/esagpos.py
+++ esag/esag/page/esagpos/esagpos.py
@@ -134,3 +134,22 @@ def get_item_group_condition(pos_profile):
         cond = "and item_group in (%s)"%(', '.join(['%s']*len(item_groups)))
 
     return cond % tuple(item_groups)
+
+@frappe.whitelist()
+def search_serial_or_batch_or_barcode_number(search_value):
+    # search barcode no
+    barcode_data = frappe.db.get_value('Item Barcode', {'barcode': search_value}, ['barcode', 'parent as item_code'], as_dict=True)
+    if barcode_data:
+        return barcode_data
+
+    # search serial no
+    serial_no_data = frappe.db.get_value('Serial No', search_value, ['name as serial_no', 'item_code'], as_dict=True)
+    if serial_no_data:
+        return serial_no_data
+
+    # search batch no
+    batch_no_data = frappe.db.get_value('Batch', search_value, ['name as batch_no', 'item as item_code'], as_dict=True)
+    if batch_no_data:
+        return batch_no_data
+
+    return {}
